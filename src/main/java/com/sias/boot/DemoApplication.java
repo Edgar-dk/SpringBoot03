348187921d3463b3bb4b2f7562c8d44ab34679b5
Edgar <Edgar@sias.com>
2022-03-29 21:11:03 +0800

    尾巴的提交

--- src/main/java/com/sias/boot/DemoApplication.java
+++ src/main/java/com/sias/boot/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 //@MapperScan("com.sias.boot.mapper")//可以不用给每一个接口标注mapper，直接一个总的标注即可
+
 @ServletComponentScan(basePackages = "com.sias.boot")//自动的把 创建好的servlet扫描进来
 @SpringBootApplication
 public class DemoApplication {
